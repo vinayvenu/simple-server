82f6f11a96b940ab6391f4c96ed07cda87eedd7d
Vinay Venu <ijkvinay@gmail.com>
2021-08-25 09:36:53 +0530

    Change domain for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name: ENV.fetch("SENDGRID_USERNAME"),
     password: ENV.fetch("SENDGRID_PASSWORD"),
-    domain: "simple.org",
+    domain: "simpledemo.samanvayfoundation.org",
     address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
